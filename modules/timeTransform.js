19533813456f13a576d1e2ddd9c4c2fc3ddb3810
Marcin Ratajczak <maratajczak@gmail.com>
2019-01-01 13:21:57 +0100

    Add npm package

---
+++ modules/timeTransform.js
@@ -0,0 +1,17 @@
+var os = require( 'os' );
+var uptime = os.uptime();
+
+
+function timeForm() {
+    var hours = Math.floor(uptime / 3600);
+    var minutes = Math.floor((uptime - (hours * 3600)) / 60);
+    var seconds = uptime - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
+exports.print = timeForm;
